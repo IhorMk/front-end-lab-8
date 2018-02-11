51618c36b1f51deb40fc5b18b38cf990507af4af
IhorMk <ihorblok@gmail.com>
2018-02-11 20:55:53 +0200

    Beautify

--- homework_06/js/task2.js
+++ homework_06/js/task2.js
@@ -1,15 +1,15 @@
-let dlr=parseFloat(prompt("enter USD:"));
-let evr=parseFloat(prompt("enter EURO:"));
-let kursE=33.2324;
-let kursD=27.1240;
-let equalDoll; 
-let equalEuro; 
+let dlr = parseFloat(prompt("enter USD:"));
+let evr = parseFloat(prompt("enter EURO:"));
+let kursE = 33.2324;
+let kursD = 27.1240;
+let equalDoll;
+let equalEuro;
 let evEqdlr;
-if ((dlr<=0)||(evr<=0)||isNaN(dlr)||isNaN(evr)){
-  console.log("Incorrect data");
-}else {
-  equalDoll=kursD*dlr;
-  equalEuro=kursE*evr;
-  evEqdlr=kursE/kursD;
-  console.log( evr+" euros are equal "+equalEuro.toFixed(2)+" UAH, "+dlr+" dollars are equal "+equalDoll.toFixed(2)+" UAH, one euro is equal "+evEqdlr.toFixed(2)+" dollars.");
+if ((dlr <= 0) || (evr <= 0) || isNaN(dlr) || isNaN(evr)) {
+    console.log("Incorrect data");
+} else {
+    equalDoll = kursD * dlr;
+    equalEuro = kursE * evr;
+    evEqdlr = kursE / kursD;
+    console.log(evr + " euros are equal " + equalEuro.toFixed(2) + " UAH, " + dlr + " dollars are equal " + equalDoll.toFixed(2) + " UAH, one euro is equal " + evEqdlr.toFixed(2) + " dollars.");
 }
