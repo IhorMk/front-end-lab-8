2ec363b264f52f4040e1d1f5963cfc1fc8e6021d
IhorMk <ihorblok@gmail.com>
2018-02-13 23:41:23 +0200

    hw7-2

--- homework_07/js/task2.js
+++ homework_07/js/task2.js
@@ -6,3 +6,9 @@ prize=10,
 totalPrize=0,
 possiblePrize = 10;
 let x=Math.floor(Math.random() * (maxNum + 1));
+if (game){
+    for (let i=0; i<attempt; i++){
+    let myNumber= prompt( "Enter a number from 0 to "+maxNum+"\n"+
+    "Attempts left: "+attempt+"\n"+"Total prize: "+totalPrize+" $"+ "\n"+
+    "Possible prize on current attempt: "+possiblePrize+" $"+"\n");
+    if (x===myNumber){
