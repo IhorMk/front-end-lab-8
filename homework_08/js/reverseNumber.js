cb7e231cd87d1d8aef5a8fe3db6c5fb0435d1243
IhorMk <ihorblok@gmail.com>
2018-02-18 20:08:15 +0200

    fixed 8.6

--- homework_08/js/reverseNumber.js
+++ homework_08/js/reverseNumber.js
@@ -1,10 +1,9 @@
 const reverseNumber = (num) => {
     let result = 0;
-    let numSign = Math.sign(num);
-    while (Math.abs(num)) {
+    while (num) {
         result *= 10;
-        result += Math.abs(num) % 10;
-        num = Math.trunc(Math.abs(num) / 10);
+        result += num % 10;
+        num = Math.trunc(num / 10);
     }
-    return result * numSign;
+    return result;
 }
